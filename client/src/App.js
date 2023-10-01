46e870475e11538021b3654e1f6875d4afb88787
Nalini Prakash <prakash9502853132@gmail.com>
2023-10-01 15:43:48 +0530

    Reinitialized repository

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacToe from './components/TIcTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
