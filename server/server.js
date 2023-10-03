bba1d4f9b1acbcdab5431a1b104472b38baa0ab0
Nalini Prakash <prakash9502853132@gmail.com>
2023-10-03 17:04:53 +0530

    vbv

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/api/game', saveGameRoute)
 
 const io = new Server( server, {
   cors: {
-    origin: "https://tic-tac-toe-client-1fru.onrender.com/",
+    origin: "https://tic-tac-toe-client-1fru.onrender.com",
     methods: ["GET", "POST"]
   }
 } );
